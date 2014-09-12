7eaceda9f31d10ecde854ef167df882bc0282c2e
Viktar Vauchkevich <victorenator@gmail.com>
2014-09-12 15:36:05 +0300

    check arg is not null

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ exports.notifyWatchdog = function() {
 };
 
 exports.watchdogUsec = function() {
-    return parseInt(process.env['WATCHDOG_USEC']) || null;
+    return parseInt(process.env['WATCHDOG_USEC'], 10) || null;
 };
 
 /**
@@ -32,15 +32,15 @@ exports.watchdogUsec = function() {
  */
 exports.startWatchdogPing = function(k) {
     if (watchdog) return;
-        
+
     var timeout = exports.watchdogUsec();
-    
+
     if (!timeout) return;
-    
+
     if (!(k > 0 && k < 1)) k = 0.5;
-    
+
     timeout = Math.round(timeout * k / 1000);
-    
+
     watchdog = setInterval(function() {
         exports.notifyWatchdog();
     }, timeout);
@@ -58,24 +58,24 @@ var Pipe = process.binding('pipe_wrap').Pipe;
 
 var origListen = net.Server.prototype.listen;
 net.Server.prototype.listen = function(arg, cb) {
-    if (typeof(arg) == 'object' && 'systemd' in arg) {
-    
+    if (typeof arg === 'object' && arg !== null && 'systemd' in arg) {
+
         if (arg.systemd >= exports.listen_fds()) {
             this.emit('listening', new Error('bad socket activation descriptor'));
-            
+
         } else {
             return this.listen({fd: exports.LISTEN_FDS_START + arg.systemd, listened: true}, cb);
         }
-        
-    } else if (typeof(arg) == 'object' && 'fd' in arg && arg.listened) {
+
+    } else if (typeof arg === 'object' && arg !== null && 'fd' in arg && arg.listened) {
         if (cb) this.once('listening', cb);
-        
+
         this._handle = new Pipe();
         this._handle.open(arg.fd);
         this._listen2(null, -1, -1);
-        
+
         this.emit('listening');
-        
+
     } else {
         origListen.apply(this, arguments);
     }

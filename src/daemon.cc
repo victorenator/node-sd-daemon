33d30e26e688797eaaf7a12404a9fba41da6815d
Viktar Vauchkevich <victorenator@gmail.com>
2018-10-09 16:31:30 +0300

    add notify_with_fds

--- src/daemon.cc
+++ src/daemon.cc
@@ -21,9 +21,41 @@ namespace daemon {
             return;
         }
 
-        v8::String::Utf8Value state(info[0]);
+        Nan::Utf8String state(info[0]);
 
         const int res = sd_notify(0, *state);
+
+        info.GetReturnValue().Set(Nan::New<v8::Integer>(res));
+    }
+
+    NAN_METHOD(notify_with_fds) {
+        Nan::HandleScope scope;
+
+        if (info.Length() != 2) {
+            Nan::ThrowTypeError("Expected two arguments");
+            return;
+        }
+
+        if (!info[0]->IsString()) {
+            Nan::ThrowTypeError("Expected string as first argument");
+            return;
+        }
+
+        if (!info[0]->IsArray()) {
+            Nan::ThrowTypeError("Expected array as second argument");
+            return;
+        }
+
+        Nan::Utf8String state(info[0]);
+
+        v8::Local<v8::Array> fdsArray = v8::Local<v8::Array>::Cast(info[1]);
+        unsigned int n_fds = fdsArray->Length();
+        int fds[n_fds];
+        for (unsigned int i = 0; i < n_fds; i++) {
+            fds[i] = v8::Local<v8::Int32>::Cast(fdsArray->Get(i))->Value();
+        }
+
+        const int res = sd_pid_notify_with_fds(0, 0, *state, fds, n_fds);
 
         info.GetReturnValue().Set(Nan::New<v8::Integer>(res));
     }
